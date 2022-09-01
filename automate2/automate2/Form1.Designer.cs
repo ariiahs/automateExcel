97abfe0b9c383b728cf893922effbb96f81e2c73
ariiahs <112620469+ariiahs@users.noreply.github.com>
2022-09-01 19:10:55 +0800

    Add files via upload

---
+++ automate2/automate2/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace automateExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Launch Excel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.openExcel);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(80, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Insert Column";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.insertColumn);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(80, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Add Formula";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.insertFormula);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Case 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Case 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Case 3";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(80, 132);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Cell Format";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.cellFormat);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(80, 213);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(138, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Update Formula";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.updateFormula);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Case 4 ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Case 5 ";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(80, 254);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(138, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Delete Column";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.deleteColumn);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(80, 295);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(138, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Delete Row";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.deleteRow);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Case 6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 300);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Case 7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(57, 19);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(136, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "TEST EXCEL FILE";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(25, 342);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Case 8";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(77, 337);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(141, 23);
+            this.button9.TabIndex = 19;
+            this.button9.Text = "Close Excel";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.closeExcel);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(254, 385);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Automation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ automate2/automate2/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace automateExcel
+{
+    public partial class Form1 : Form
+    {
+        Excel.Application oXL;
+        Excel._Workbook oWB;
+        Excel._Worksheet oSheet;
+        Excel.Range oRng;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void openExcel(object sender, System.EventArgs e)
+        {
+           try
+            {
+                // Verify Launching of Excel
+                oXL = new Excel.Application();
+                oXL.Visible = true;
+
+                //Verify creation of new WorkBook
+                oWB = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
+
+                //Verify creation of Worksheet
+                oSheet = (Excel._Worksheet)oWB.ActiveSheet;
+                
+                //Verify adding of new columns
+                oSheet.Cells[1, 1] = "First Name";
+                oSheet.Cells[1, 2] = "Last Name";
+                oSheet.Cells[1, 3] = "Full Name";
+                oSheet.Cells[1, 4] = "Monthly Salary";
+
+                // Create an array to multiple values at once.
+                string[,] empName = new string[4, 2];
+
+                empName[0, 0] = "Adam";
+                empName[0, 1] = "Cruz";
+                empName[1, 0] = "Braedyn";
+                empName[1, 1] = "Lee";
+                empName[2, 0] = "Cherry";
+                empName[2, 1] = "Tan";
+                empName[3, 0] = "Diane";
+                empName[3, 1] = "Jones";
+
+                //Fill A2:B6 with an array of values (First and Last Names).
+                oSheet.get_Range("A2", "B5").Value2 = empName;
+                
+                // Verify if Full name displays the FirstName and Last Name of a person
+                oRng = oSheet.get_Range("C2", "C5");
+                oRng.Formula = "=A2 & \" \" & B2";
+
+                //Validate format for displayed values in monthly Salary
+                oRng = oSheet.get_Range("D2", "D5");
+                oRng.EntireColumn.AutoFit();
+                oRng.NumberFormat = "$0.00";
+                oSheet.Cells[2, 4] = "80000";
+                oSheet.Cells[3, 4] = "40000";
+                oSheet.Cells[4, 4] = "50000";
+                oSheet.Cells[5, 4] = "60000";
+
+                //Make sure Excel is visible and give the user control
+                oXL.Visible = true;
+                oXL.UserControl = true;
+
+           }
+
+                catch (Exception theException)
+                {
+                    String errorMessage;
+                    errorMessage = "Error: ";
+                    errorMessage = String.Concat(errorMessage, theException.Message);
+                    errorMessage = String.Concat(errorMessage, " Line: ");
+                    errorMessage = String.Concat(errorMessage, theException.Source);
+
+                    MessageBox.Show(errorMessage, "Error");
+                }
+        }
+
+        private void insertColumn(object sender, EventArgs e)
+        {
+            //Verify inserting of new columns at the end of the record
+            oSheet.Cells[1, 5] = "Annual Salary";
+            oRng.NumberFormat = "$0.00";
+        }
+
+        private void cellFormat(object sender, EventArgs e)
+        {
+            //Format Columns
+            oSheet.get_Range("A1", "E1").Font.Bold = true;
+            oSheet.get_Range("A1", "E1").VerticalAlignment =
+            Excel.XlVAlign.xlVAlignCenter;
+
+            //AutoFit columns A:E.
+            oRng = oSheet.get_Range("A1", "E1");
+            oRng.EntireColumn.AutoFit();
+        }
+
+        private void insertFormula(object sender, EventArgs e)
+        {
+            //Verify total salary of each person for 1 year (monthly salary * 12 months)
+            oRng = oSheet.get_Range("E2", "E5");
+            oRng.Formula = "=D2 * 12";
+            oRng.NumberFormat = "$0.00";
+        }
+
+        private void updateFormula(object sender, EventArgs e)
+        {
+            //Verify total salary when formula is updated to 2 years (monthly salary * 24 months)
+            oRng = oSheet.get_Range("E2", "E5");
+            oRng.Formula = "=D2 * 24";
+            oRng.NumberFormat = "$0.00";
+
+        }
+
+        private void deleteColumn(object sender, EventArgs e)
+        {
+            //Verify if delete column button will delete the newly added Column
+            oRng = oSheet.get_Range("E1", "E5");
+            oRng.Delete();
+        }
+
+        private void deleteRow(object sender, EventArgs e)
+        {
+            //Verify if delete row button will delete a row
+            oRng = oSheet.get_Range("A5", "E5");
+            oRng.Delete();
+        }
+
+        private void closeExcel(object sender, EventArgs e)
+        {
+            //Verify closing of excel app 
+            oXL.Quit();
+            Application.Exit();
+
+        }
+  
+    }
+}   
+
+            
+       
+           
+
+
